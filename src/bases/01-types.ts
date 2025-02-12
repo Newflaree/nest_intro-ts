6d256ff54dab86f3af8149c4ef2eb7e1a8c2e5f1
Camilo López <camiloj.lopez79@gmail.com>
2025-02-12 16:21:47 -0300

    Tipos de datos - continuación

--- src/bases/01-types.ts
+++ src/bases/01-types.ts
@@ -1,3 +1,17 @@
-export const name: string = 'Camilo';
+export let name: string = 'Camilo';
 export const age: number = 32;
 export const isValid: boolean = true;
+
+name = 'Javier';
+
+export const templateString = `Esto es un string
+multilínea
+que puede tener
+" dobles
+' simple
+iyectar valores ${ name }
+expresiones ${ 1 + 1 }
+números: ${ age }
+booleans: ${ isValid }`;
+
+console.log( templateString );
