5968c0498439b3a97b5a1633478a8ba002ef18a5
Camilo López <camiloj.lopez79@gmail.com>
2025-02-12 16:33:17 -0300

    Objetos e interfaces

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
-import { name, age } from './bases/01-types';
+//import { name, age } from './bases/01-types';
+import { pokemonIds, bulbasaur } from './bases/02-objects';
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
@@ -14,7 +15,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>${ name } - ${ age }</h1>
+    <h1>${ bulbasaur.name }</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
