728ea7493240ad2f8167f617c667f63fb966e580
Camilo López <camiloj.lopez79@gmail.com>
2025-02-12 18:15:42 -0300

    Getters, métodos y this

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,8 +1,25 @@
 export class Pokemon {
+  get imageUrl(): string {
+    return `https://pokemon.com/${ this.id }.jpg`;
+  }
+
   constructor(
     public readonly id: number,
-    public name: string
+    public name: string,
   ) {}
+
+  public scream() {
+    console.log( `${ this.name.toUpperCase() }!!!` );
+  }
+
+  public speak() {
+    console.log( `${ this.name }, ${ this.name }` );
+  }
 }
 
 export const charmander = new Pokemon( 4, 'Charmander' );
+
+console.log( charmander );
+
+charmander.scream();
+charmander.speak();
